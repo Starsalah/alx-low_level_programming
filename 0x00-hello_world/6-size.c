70a6c56b89ceb2876401586e725396b469520be3
Starsalah <taiye9193@gmail.com>
2023-03-16 16:57:49 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(void) { printf("char:%lu\nint:%lu\nlong:%lu\nlong long:%lu\nfloat:%lu\n", sizeof(char), sizeof(int), sizeof(long), sizeof(long long), sizeof(float)); return 0; }
