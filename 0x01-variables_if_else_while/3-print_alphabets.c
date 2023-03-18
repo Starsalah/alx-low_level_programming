69f35dd654801fc2eb4c056a73ffa825d39692d9
Starsalah <taiye9193@gmail.com>
2023-03-18 06:49:47 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    /* Print lowercase letters */
+    for (i = 97; i <= 122; i++)
+    {
+        putchar(i);
+    }
+
+    /* Print uppercase letters */
+    for (i = 65; i <= 90; i++)
+    {
+        putchar(i);
+    }
+
+    /* Print newline character */
+    putchar('\n');
+
+    return (0);
+}
