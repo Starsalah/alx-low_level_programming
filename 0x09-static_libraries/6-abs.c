9fd9a82458693de297563ca3fa7edbc40177d37c
Starsalah <taiye9193@gmail.com>
2023-04-07 15:40:11 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - gets an integer's absolute value
+ * @n: the integer to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+The aim of this project is to create a library libmy.a containing all the functions use din previous projects in this repository.
